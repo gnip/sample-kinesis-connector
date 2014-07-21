1b16b896731d27308f109af9e85580d5c938e0d7
Nick Isaacs <isaacs@twitter.com>
2014-07-21 16:46:42 -0600

    remove connection package

--- src/main/java/com/twitter/kinesis/connection/GnipConnectionException.java
+++
@@ -1,7 +0,0 @@
-package com.twitter.kinesis.connection;
-
-public class GnipConnectionException extends Exception {
-    public GnipConnectionException(String message) {
-        super(message);
-    }
-}
